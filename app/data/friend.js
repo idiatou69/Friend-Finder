7f451806dc796198cc00eb7d8060ab1679a98f85
idiatou <idiatoudiallo1741@gmail.com>
2019-07-23 22:04:35 -0500

    update

---
+++ app/data/friend.js
@@ -0,0 +1,56 @@
+
+
+var waitingArray=[
+{
+  "name":"Ahmed",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+{
+  "name":"marie",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      3,
+      2,
+      6,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+{
+  "name":"abdoul",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      4,
+      1,
+      5,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}
+]
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = waitingArray;
+  
---
+++ routing/apiRoutes.js
@@ -0,0 +1,84 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friendData = require("../app/data/friend");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function (app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friend", function (req, res) {
+    res.json(friendData);
+  });
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/friend", (req, res) => {
+    var userScore = req.body.scores;
+    console.log("userscore",req.body);
+    var scoresArr = [];
+    var bestMatch = 0;
+
+    for (var i = 0; i < friendData.length; i++) {
+      var scoreDiff = 0;
+
+      for (var j = 0; j < userScore.length; j++) {
+        scoreDiff += (Math.abs(parseInt(friendData[i].scores[j]) - parseInt(userScore[j])))
+      }
+      scoresArr.push(scoreDiff);
+    }
+    for (var i = 0; i < scoresArr.length; i++) {
+      if (scoresArr[i] <= scoresArr[bestMatch]) {
+        bestMatch = i;
+      }
+    }
+    //return best match//
+    var soulMate = friendData[bestMatch];
+    res.json(soulMate);
+
+    console.log(req.body);
+    friendData.push(req.body)
+  });
+
+  // app.post("/api/friend", function (req, res) {
+  //   // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+  //   // It will do this by sending out the value "true" have a table
+  //   // req.body is available since we're using the body parsing middleware
+  //   if (friendData.length < 5) {
+  //     friendData.push(req.body);
+  //     res.json(true);
+  //   }
+  //   else {
+  //     waitListData.push(req.body);
+  //     res.json(false);
+  //   }
+  // });
+
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+  app.post("/api/clear", function (req, res) {
+    // Empty out the arrays of data
+    friendData.length = [];
+
+
+    res.json({ ok: true });
+  });
+};
